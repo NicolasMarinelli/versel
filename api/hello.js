d77bbd30989179dbb7dff92d98ba48ba30b7a97e
Nicolas Marinelli <marinellinicolas@gmail.com>
2024-12-27 14:10:45 -0300

    second commit

--- api/hello.js
+++ api/hello.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "¡Hola, desde Express!" });
+  res.json({ message:[1,2,3,4,5,6] });
 });
 
 module.exports = app;
