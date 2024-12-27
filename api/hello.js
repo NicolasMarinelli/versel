2176ba508977afa15d644b55fc7d7af01093f4f4
Nicolas Marinelli <marinellinicolas@gmail.com>
2024-12-27 13:52:40 -0300

    miprimerversel

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "¡Hola, desde Express!" });
-});
-
-module.exports = app;
+export default function handler(req, res) {
+    res.status(200).json({ message: "¡Hola, mundo!" });
+  }
